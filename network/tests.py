259fbf9ec33ab0e4d30df7946e6c8674dc76c738
Matheus <matheus1250.mc@gmail.com>
2020-10-31 20:09:00 -0300

    basic tests for views

--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from .models import User
 
 # Create your tests here.
+class ViewsTestCase(TestCase):
+	client = Client()
+
+	def setUp(self):
+		mock_user = User.objects.create_user(username='test_user', password='12345')
+
+	def test_get_index(self):
+		response = self.client.get(reverse('index'))
+		self.assertEqual(response.status_code, 200)
+	
+	def test_get_login_view(self):
+		response = self.client.get(reverse('login'))
+		self.assertEqual(response.status_code, 200)
+	
+	def test_post_login_view(self):
+		data = {
+			'username': 'test_user',
+			'password': '12345'
+		}
+
+		response = self.client.post(reverse('login'), data)
+
+		# for user registered, the login should be successful, without warning message
+		try:
+			warning_message = response.context['message']
+			# no warning message == TypeError
+		except TypeError:
+			warning_message = False
+
+		self.assertEqual(warning_message, False)
