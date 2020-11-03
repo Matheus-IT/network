33c280b9bba4a2019dbcc8155929fc899121f437
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2020-11-03 11:02:33 -0300

    tests for register view

--- network/tests.py
+++ network/tests.py
@@ -8,7 +8,18 @@ class ViewsTestCase(TestCase):
 	client = Client()
 
 	def setUp(self):
-		mock_user = User.objects.create_user(username='test_user', password='12345')
+		mock_user = User.objects.create_user(
+			username='test_user', password='12345', email='test_user@example.com'
+		)
+	
+	def hasWarning(self, response):
+		# for registered users, the login should be successful, without warning message
+		try:
+			warning_message = response.context['message']
+			# no warning message == TypeError
+		except TypeError:
+			warning_message = False
+		return warning_message
 
 	def test_get_index(self):
 		response = self.client.get(reverse('index'))
@@ -26,13 +37,7 @@ class ViewsTestCase(TestCase):
 
 		response = self.client.post(reverse('login'), data)
 
-		# for registered users, the login should be successful, without warning message
-		try:
-			warning_message = response.context['message']
-			# no warning message == TypeError
-		except TypeError:
-			warning_message = False
-
+		warning_message = self.hasWarning(response)
 		self.assertFalse(warning_message)
 	
 	def test_fail_post_login_view(self):
@@ -43,19 +48,38 @@ class ViewsTestCase(TestCase):
 
 		response = self.client.post(reverse('login'),  data)
 
-		try:
-			warning_message = response.context['message']
-		except TypeError:
-			warning_message = False
-		
+		warning_message = self.hasWarning(response)
 		self.assertTrue(warning_message)
 	
 	def test_logout_view(self):
-		response = self.client.get(reverse('logout'))
-
-		# I don't know why is 302, but...
-		self.assertEqual(response.status_code, 302)
+		response = self.client.get(reverse('logout'), follow=True)
+		self.assertEqual(response.status_code, 200)
 	
 	def test_get_register_view(self):
 		response = self.client.get(reverse('register'))
 		self.assertEqual(response.status_code, 200)
+	
+	def test_post_register_view(self):
+		data = {
+			'username': 'new_test_user',
+			'email': 'test@example.com',
+			'password': '12345',
+			'confirmation': '12345'
+		}
+
+		response = self.client.post(reverse('register'), data, follow=True)
+		self.assertEqual(response.status_code, 200)
+	
+	def test_fail_post_register_view(self):
+		data = {
+			'username': 'test_user',
+			'email': 'test_user@example.com',
+			'password': '12345',
+			'confirmation': '12345'
+		}
+
+		response = self.client.post(reverse('register'), data, follow=True)
+
+		warning_message = self.hasWarning(response)
+		
+		self.assertTrue(warning_message)
