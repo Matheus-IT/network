cc4a4677fe65b96f705b7576fe25d8c25cea035a
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2020-11-05 11:25:40 -0300

    new module Post

--- network/models.py
+++ network/models.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.forms.widgets import Textarea
 
 # adicional models: posts, likes, and followers
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now=True)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'Auth {self.poster.username}: {self.content[:30]}... Timestamp: {self.timestamp}'
