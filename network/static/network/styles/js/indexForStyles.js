71f0d5a4fd12092c445818dbd2aebf8d1e304731
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2020-12-01 14:02:02 -0300

    tweaks on indexForStyles.js

--- network/static/network/styles/js/indexForStyles.js
+++ network/static/network/styles/js/indexForStyles.js
@@ -2,15 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
 	const newPost = document.querySelector('#newPost');
 	const postButton = document.querySelector('#submitNewPost');
 
-	const borderRadiusBefore = newPost.style.borderRadius;
-
-	newPost.addEventListener('focus', function() {
-		postButton.style.alignSelf = 'flex-end';
-		newPost.style.borderRadius = '0.4em';
-	});
-
-	newPost.addEventListener('focusout', function() {
-		postButton.style.alignSelf = 'center';
-		newPost.style.borderRadius = borderRadiusBefore;
-	});
+	if (newPost) {
+		const borderRadiusBefore = newPost.style.borderRadius;
+	
+		newPost.addEventListener('focus', function() {
+			postButton.style.alignSelf = 'flex-end';
+			newPost.style.borderRadius = '0.4em';
+		});
+	
+		newPost.addEventListener('focusout', function() {
+			postButton.style.alignSelf = 'center';
+			newPost.style.borderRadius = borderRadiusBefore;
+		});
+	}
 });
